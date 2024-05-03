ba95c777253347c8f219ca6ff0de686c4faf6c5b
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-03 19:54:13 +0530

    Drag & Drop Connected Sorting Group

--- apps/blocks-world/src/app/app.component.ts
+++ apps/blocks-world/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+import { WorldComponent } from '@blocks-world-monorepo/world';
+
 @Component({
   standalone: true,
-  imports: [],
+  imports: [WorldComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- libs/world/src/lib/components/world/world.component.ts
+++ libs/world/src/lib/components/world/world.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'lib-world',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CdkDropListGroup, CdkDropList, CdkDrag],
   templateUrl: './world.component.html',
   styleUrl: './world.component.scss',
 })
-export class WorldComponent {}
+export class WorldComponent {
+  column1: string[] = [];
+  column2: string[] = [];
+  column3: string[] = [];
+  column4: string[] = [];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer !== event.container) {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+}
