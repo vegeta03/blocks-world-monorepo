6235c243f0fd808de58b5c35acc4041a28f18b01
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-06 01:41:18 +0530

    user-interface lib

---
+++ libs/user-interface/src/lib/user-interface/user-interface.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-user-interface',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-interface.component.html',
+  styleUrl: './user-interface.component.scss',
+})
+export class UserInterfaceComponent {}
