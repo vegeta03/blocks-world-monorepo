130361a700c578bba44086929c93c3f0a4ee6634
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-06 20:28:47 +0530

    QueryWindowComponent

--- libs/query-window/src/lib/components/query-window/query-window.component.ts
+++ libs/query-window/src/lib/components/query-window/query-window.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { UserMessageComponent } from '../user-message/user-message.component';
 import { SystemMessageComponent } from '../system-message/system-message.component';
@@ -7,8 +10,26 @@ import { SystemMessageComponent } from '../system-message/system-message.compone
 @Component({
   selector: 'lib-query-window',
   standalone: true,
-  imports: [CommonModule, UserMessageComponent, SystemMessageComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    UserMessageComponent,
+    SystemMessageComponent,
+  ],
   templateUrl: './query-window.component.html',
   styleUrl: './query-window.component.scss',
 })
-export class QueryWindowComponent {}
+export class QueryWindowComponent implements AfterViewInit {
+  @ViewChild('queryWindow', { static: true }) queryWindow!: ElementRef;
+
+  ngAfterViewInit(): void {
+    try {
+      this.queryWindow.nativeElement.scrollTop =
+        this.queryWindow.nativeElement.scrollHeight;
+    } catch (err) {
+      console.error('Error scrolling:', err);
+    }
+  }
+}
